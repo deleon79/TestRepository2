92c9ee5b2ceba47326d581f3bcb430495868dee3
Jose A. Deleon <deleon@fordham.edu>
2016-07-28 14:17:43 -0400

    Adding new Functionality To the branch.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Change from Browser");
         System.out.println("Merge Issue");
+        System.out.println("These are New Branch Changes");
     }
 }
